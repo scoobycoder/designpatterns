c0f498dce06645e4995d0bc79cf698304f61e92b
scoobycoder <scoobycoder@gmail.com>
2014-05-08 17:58:05 -0400

    committing code so that I can work on it from home and add behavior test with mockito

--- test/halterman/command/CommandPatternTest.java
+++ test/halterman/command/CommandPatternTest.java
@@ -1,12 +1,20 @@
 package halterman.command;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class CommandPatternTest {
 
 	@Test
 	public void shouldTurnOnLight() {
+		Light l = new Light();
+		Command loc = new LightOnCommand(l);
 		
+		RemoteControl rc = new RemoteControl();
+		rc.setCommand(loc);
+		
+		// Need Mockito here, will work on this from home.
 	}
 	
 	
