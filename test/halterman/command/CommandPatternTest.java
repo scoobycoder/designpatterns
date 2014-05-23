16ded678ffe59a4e5bef2a501ba8176f944b82f9
scoobycoder <scoobycoder@gmail.com>
2014-05-23 09:11:13 -0400

    starting point for code club kata

--- test/halterman/command/CommandPatternTest.java
+++ test/halterman/command/CommandPatternTest.java
@@ -1,38 +1,6 @@
 package halterman.command;
 
-import static org.mockito.Mockito.verify;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
-
 public class CommandPatternTest {
 
-	@InjectMocks 
-	private Switch lightSwitch;
-
-	@Mock
-	Light light;
-	
-	@Spy
-	Command lightsOn = new LightOnCommand(light);
-	
-	@Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        lightSwitch = new Switch();
-        lightSwitch.setCommand(lightsOn);
-    }
-	
-	@Test
-	public void shouldTurnOnLight() {
-		lightSwitch.pressButton();
-		
-		verify(light).switchOn();
-	}
-	
 	
 }
